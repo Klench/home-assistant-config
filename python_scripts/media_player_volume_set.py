e2037c3aea336075317b9c0b23221f49bb5b3db4
Erik Thorsell <erithor@student.chalmers.se>
2020-07-19 21:38:43 +0200

    Modify media player volume to handle multiple speakers

--- python_scripts/media_player_volume_set.py
+++ python_scripts/media_player_volume_set.py
@@ -1,17 +1,32 @@
 entity_id = data.get('entity_id')
-initial_volume_level = hass.states.get(entity_id).attributes['volume_level']
-volume_reference = float(data.get("volume_level"))
-volume_diff = volume_reference - initial_volume_level
-volume_diff_sign = volume_diff/abs(volume_diff)
+group_volume = bool(data.get('group_volume'))
+target_volume = float(data.get('volume_level'))
+volume_increment = 0.015
 
-volume_increment = 0.015 * volume_diff_sign
-volume_level = initial_volume_level
+if group_volume:
+  entity_id_list = hass.states.get(entity_id).attributes['sonos_group']
+else:
+  entity_id_list = [entity_id]
 
-while ((volume_level + volume_increment < volume_reference) and (volume_diff_sign > 0)) or ((volume_level + volume_increment > volume_reference) and (volume_diff_sign < 0)):
-    volume_level = volume_level + volume_increment
-    service_data = {"entity_id": entity_id, "volume_level": volume_level}
+entity_volume_synced = [False] * len(entity_id_list)
+
+while entity_volume_synced.count(False):
+
+  for speaker_entity_id in entity_id_list:
+    current_volume = hass.states.get(speaker_entity_id).attributes['volume_level']
+
+    volume_diff = target_volume - current_volume
+
+    if abs(volume_diff) <= volume_increment:
+      entity_volume_synced[entity_id_list.index(speaker_entity_id)] = True
+      service_data = {"entity_id": speaker_entity_id, "volume_level": target_volume}
+      hass.services.call("media_player", "volume_set", service_data)
+      continue
+
+    volume_diff_sign = volume_diff/abs(volume_diff)
+    new_volume = current_volume + volume_increment * volume_diff_sign
+
+    service_data = {"entity_id": speaker_entity_id, "volume_level": new_volume}
     hass.services.call("media_player", "volume_set", service_data)
-    time.sleep(.1)
 
-service_data_reference = {"entity_id": entity_id, "volume_level": volume_reference}
-hass.services.call("media_player", "volume_set", service_data_reference)
+    time.sleep(.1)

2f328d4ba4b25183abb88a71604a860782fe05d8
David Stenman <david.stenman@me.com>
2021-02-21 22:48:36 +0100

    Modify media_player_volume_set python script so it will not fetch the actual volume from the entities at each loop iteration and use a saved volume level instead.

--- python_scripts/media_player_volume_set.py
+++ python_scripts/media_player_volume_set.py
@@ -1,32 +1,38 @@
 entity_id = data.get('entity_id')
 target_volume = float(data.get('volume_level'))
 set_group_volume = bool(data.get('set_group_volume'))
-volume_increment = 0.025
+volume_increment = 0.003
 
 if set_group_volume:
-  speaker_entity_id_list = hass.states.get(entity_id).attributes['sonos_group']
+  entity_id_list = hass.states.get(entity_id).attributes['sonos_group']
 else:
-  speaker_entity_id_list = [entity_id]
+  entity_id_list = [entity_id]
 
-speaker_entity_dict = {speaker_entity_id: False for speaker_entity_id in speaker_entity_id_list}
+entity_current_volume_list = []
+entity_volume_is_set_list = []
 
-while False in speaker_entity_dict.values():
+for entity_id in entity_id_list:
+  entity_current_volume_list.append(hass.states.get(entity_id).attributes['volume_level'])
+  entity_volume_is_set_list.append(False)
 
-  for speaker_entity_id in speaker_entity_id_list:
-    current_volume = hass.states.get(speaker_entity_id).attributes['volume_level']
+while not all(entity_volume_is_set_list):
 
-    volume_diff = target_volume - current_volume
+  for i in range(len(entity_id_list)):
+
+    volume_diff = target_volume - entity_current_volume_list[i]
 
     if abs(volume_diff) <= volume_increment:
-      speaker_entity_dict[speaker_entity_id] = True
-      service_data = {"entity_id": speaker_entity_id, "volume_level": target_volume}
+      entity_volume_is_set_list[i] = True
+      service_data = {"entity_id": entity_id_list[i], "volume_level": target_volume}
       hass.services.call("media_player", "volume_set", service_data)
       continue
 
     volume_diff_sign = volume_diff/abs(volume_diff)
-    new_volume = current_volume + volume_increment * volume_diff_sign
+    new_volume = entity_current_volume_list[i] + volume_increment * volume_diff_sign
 
-    service_data = {"entity_id": speaker_entity_id, "volume_level": new_volume}
+    service_data = {"entity_id": entity_id_list[i], "volume_level": new_volume}
     hass.services.call("media_player", "volume_set", service_data)
 
-  time.sleep(.08)
+    entity_current_volume_list[i] = new_volume
+
+  time.sleep(.01)
